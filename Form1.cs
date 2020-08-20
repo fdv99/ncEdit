a164c39de85dc793de9b238de285978b0659ee2a
V6-Engineering <forrest@v6carolina.com>
2020-08-20 10:35:23 -0400

    added comments

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,7 @@ namespace ncEdit
 {
     public partial class Form1 : Form
     {
-        private string filetext = string.Empty;
+
         private List<string> fileList = new List<string>();
         private string ncFileName = string.Empty;
         public Form1()
@@ -32,9 +32,8 @@ namespace ncEdit
             {
                 if ((myStream = openFileDialog1.OpenFile()) !=null)
                 {
-                    ncFileName = openFileDialog1.FileName;
-                    string filetext = File.ReadAllText(ncFileName);
-                    original_code.Text = filetext;
+                    ncFileName = openFileDialog1.FileName;  //saves file path
+                    original_code.Text = File.ReadAllText(ncFileName);  //displays original code in Text box
                 }
             }
         }
@@ -42,14 +41,14 @@ namespace ncEdit
         private void btn_ConvertNC_Click(object sender, EventArgs e)
         {
 
-            List<string> convertList = File.ReadAllLines(ncFileName).ToList();
-            int convertLength = convertList.Count;
-            convertList.RemoveAt(convertLength - 1);
-            convertList.RemoveAt(convertLength - 2);
-            convertList.Add("G00X120.8661Y61.0236Z3.937");
-            convertList.Add("M707");
-            convertList.Add("G50");
-            converted_code.Lines = convertList.ToArray();
+            List<string> convertList = File.ReadAllLines(ncFileName).ToList();  //converts original code to List
+            int convertLength = convertList.Count;  //gets number of items in List
+            convertList.RemoveAt(convertLength - 1);  //deletes last row of list (%)
+            convertList.RemoveAt(convertLength - 2);  //deletes second last row of list (G50)
+            convertList.Add("G00X120.8661Y61.0236Z3.937");  //append go home on end of list
+            convertList.Add("M707");  //append shuttle command on end
+            convertList.Add("G50");  //append G50 on end
+            converted_code.Lines = convertList.ToArray();  //display new code in text box
 
             string AddSuffix(string filename, string suffix)
             {
@@ -59,9 +58,9 @@ namespace ncEdit
                 return Path.Combine(fDir, String.Concat(fName, suffix, fExt));
             }
 
-            string newFileName = AddSuffix(ncFileName, String.Format("({0})", "F1"));
+            string newFileName = AddSuffix(ncFileName, String.Format("({0})", "F1")); //append (F1) to file name
 
-            converted_code.SaveFile(newFileName, RichTextBoxStreamType.RichText);
+            converted_code.SaveFile(newFileName, RichTextBoxStreamType.RichText);  //save new file
 
 
 
