65bf3e14a795e7368aa21547b1631cbb3cf2bf87
V6-Engineering <forrest@v6carolina.com>
2020-08-20 10:30:29 -0400

    Saving works.  Edits at bottom work.  Need to work on top rows.

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,7 +15,8 @@ namespace ncEdit
     public partial class Form1 : Form
     {
         private string filetext = string.Empty;
-        private string tempText = string.Empty;
+        private List<string> fileList = new List<string>();
+        private string ncFileName = string.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -25,12 +27,13 @@ namespace ncEdit
             Stream myStream;
 
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "nc files (*.nc)|*.nc|All files (*.*)|*.*";
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 if ((myStream = openFileDialog1.OpenFile()) !=null)
                 {
-                    string strfilename = openFileDialog1.FileName;
-                    filetext = File.ReadAllText(strfilename);
+                    ncFileName = openFileDialog1.FileName;
+                    string filetext = File.ReadAllText(ncFileName);
                     original_code.Text = filetext;
                 }
             }
@@ -38,11 +41,28 @@ namespace ncEdit
 
         private void btn_ConvertNC_Click(object sender, EventArgs e)
         {
-            //string convertText = filetext;
-            filetext.Replace("E10", "hey");
-            File.WriteAllText(@"C:\Users\V6-Engineering\Desktop\temp.nc", filetext);
-            tempText = File.ReadAllText(@"C:\Users\V6-Engineering\Desktop\temp.nc");
-            converted_code.Text = tempText;
+
+            List<string> convertList = File.ReadAllLines(ncFileName).ToList();
+            int convertLength = convertList.Count;
+            convertList.RemoveAt(convertLength - 1);
+            convertList.RemoveAt(convertLength - 2);
+            convertList.Add("G00X120.8661Y61.0236Z3.937");
+            convertList.Add("M707");
+            convertList.Add("G50");
+            converted_code.Lines = convertList.ToArray();
+
+            string AddSuffix(string filename, string suffix)
+            {
+                string fDir = Path.GetDirectoryName(ncFileName);
+                string fName = Path.GetFileNameWithoutExtension(ncFileName);
+                string fExt = Path.GetExtension(ncFileName);
+                return Path.Combine(fDir, String.Concat(fName, suffix, fExt));
+            }
+
+            string newFileName = AddSuffix(ncFileName, String.Format("({0})", "F1"));
+
+            converted_code.SaveFile(newFileName, RichTextBoxStreamType.RichText);
+
 
 
         }
