f69d045b82edfd1fda7708d01150f0006a86fb43
V6-Engineering <forrest@v6carolina.com>
2020-08-21 09:57:34 -0400

    formatting changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,12 +36,24 @@
             this.converted_code = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Btn_OpenFile
             // 
             this.Btn_OpenFile.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_OpenFile.Location = new System.Drawing.Point(15, 12);
+            this.Btn_OpenFile.Location = new System.Drawing.Point(12, 12);
             this.Btn_OpenFile.Name = "Btn_OpenFile";
             this.Btn_OpenFile.Size = new System.Drawing.Size(85, 23);
             this.Btn_OpenFile.TabIndex = 0;
@@ -52,7 +64,7 @@
             // Btn_ConvertNC
             // 
             this.Btn_ConvertNC.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_ConvertNC.Location = new System.Drawing.Point(433, 12);
+            this.Btn_ConvertNC.Location = new System.Drawing.Point(106, 12);
             this.Btn_ConvertNC.Name = "Btn_ConvertNC";
             this.Btn_ConvertNC.Size = new System.Drawing.Size(96, 23);
             this.Btn_ConvertNC.TabIndex = 1;
@@ -63,7 +75,7 @@
             // ck_ShuttleTable
             // 
             this.ck_ShuttleTable.AutoSize = true;
-            this.ck_ShuttleTable.Location = new System.Drawing.Point(15, 45);
+            this.ck_ShuttleTable.Location = new System.Drawing.Point(12, 41);
             this.ck_ShuttleTable.Name = "ck_ShuttleTable";
             this.ck_ShuttleTable.Size = new System.Drawing.Size(92, 17);
             this.ck_ShuttleTable.TabIndex = 2;
@@ -73,30 +85,33 @@
             // original_code
             // 
             this.original_code.BackColor = System.Drawing.SystemColors.Desktop;
+            this.original_code.Dock = System.Windows.Forms.DockStyle.Fill;
             this.original_code.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.original_code.ForeColor = System.Drawing.Color.White;
-            this.original_code.Location = new System.Drawing.Point(11, 84);
+            this.original_code.Location = new System.Drawing.Point(0, 0);
             this.original_code.Name = "original_code";
-            this.original_code.Size = new System.Drawing.Size(388, 415);
+            this.original_code.Size = new System.Drawing.Size(321, 390);
             this.original_code.TabIndex = 4;
             this.original_code.Text = "";
             // 
             // converted_code
             // 
             this.converted_code.BackColor = System.Drawing.SystemColors.Desktop;
+            this.converted_code.Dock = System.Windows.Forms.DockStyle.Fill;
             this.converted_code.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.converted_code.ForeColor = System.Drawing.Color.White;
-            this.converted_code.Location = new System.Drawing.Point(433, 84);
+            this.converted_code.Location = new System.Drawing.Point(0, 0);
             this.converted_code.Name = "converted_code";
-            this.converted_code.Size = new System.Drawing.Size(389, 415);
+            this.converted_code.Size = new System.Drawing.Size(319, 390);
             this.converted_code.TabIndex = 5;
             this.converted_code.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(8, 65);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 18);
             this.label1.TabIndex = 6;
@@ -105,30 +120,84 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
             this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(430, 65);
+            this.label2.Location = new System.Drawing.Point(0, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(140, 18);
             this.label2.TabIndex = 7;
             this.label2.Text = "Converted NC File";
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 100);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.original_code);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.converted_code);
+            this.splitContainer1.Size = new System.Drawing.Size(644, 390);
+            this.splitContainer1.SplitterDistance = 321;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 70);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Size = new System.Drawing.Size(644, 30);
+            this.splitContainer2.SplitterDistance = 322;
+            this.splitContainer2.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Btn_OpenFile);
+            this.panel1.Controls.Add(this.Btn_ConvertNC);
+            this.panel1.Controls.Add(this.ck_ShuttleTable);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(644, 70);
+            this.panel1.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 511);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.converted_code);
-            this.Controls.Add(this.original_code);
-            this.Controls.Add(this.ck_ShuttleTable);
-            this.Controls.Add(this.Btn_ConvertNC);
-            this.Controls.Add(this.Btn_OpenFile);
+            this.ClientSize = new System.Drawing.Size(644, 490);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.splitContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(608, 529);
             this.Name = "Form1";
             this.Text = "V6 NC Conversion";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -141,6 +210,9 @@
         private System.Windows.Forms.RichTextBox converted_code;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
